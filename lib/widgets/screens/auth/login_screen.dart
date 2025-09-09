95994bc1a3c4b7de3c4530b00b6b08a709ddc823
GWANWOO KIM <gwk1125@hanyang.ac.kr>
2025-09-09 11:28:35 +0900

    Remove unused url_launcher import from login screen

--- lib/widgets/screens/auth/login_screen.dart
+++ lib/widgets/screens/auth/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 import '../../../core/constants/colors.dart';
 import '../../../main.dart';
 
